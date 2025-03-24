c02aa7e16b7034b44c7bddc1ad847ffd956520ee
LimeGG <setet97@gmail.com>
2025-03-24 10:44:15 +0300

    Удалил сломанный submodel

---
+++ cmd/app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
